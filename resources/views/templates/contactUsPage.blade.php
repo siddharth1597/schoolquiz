5aa225076d9683ee180014cb948407bacb9e8ef5
Siddharth Rastogi <sidd15597@gmail.com>
2021-07-20 17:21:17 +0530

    last fixes

--- resources/views/templates/contactUsPage.blade.php
+++ resources/views/templates/contactUsPage.blade.php
@@ -3,10 +3,8 @@
         <i id="fa-back" class="d-none fas fa-arrow-left h2 text-dark position-absolute cursor-pointer" title="Go to Dashboard"></i>
         <div class="d-flex justify-between">
             <div class="d-flex">
-                <i id="fa-edit" class="fas fa-id-card h2 text-dark mr-3 my-auto""></i>
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight my-auto">
-                    {{ __('Contact us') }}
-                </h2>
+                <img class="winner_page_image mt-4 rounded-lg m-auto" src="{{ url('/images/School_Quiz_Logo.jpg') }}" alt="logo">
+                <button id="download_app" class="btn btn-primary px-3 my-auto ml-2">Download App</button>
             </div>
             <div class="invisible">
                 Go to Dashboard
@@ -16,7 +14,6 @@
             @endphp
             <div class="d-flex">
                 <span class="home_icon badge badge-dark mr-2 my-auto">Home</span>
-                <img class="logo home_icon" src="{{ url($icon) }}" alt="School Quiz"  title="Go to Home Screen">
             </div>
         </div>
     </x-slot>
@@ -28,24 +25,24 @@
             @endphp
             <div class="card shadow border border-white w-100 contact-us-back">
                 <div class="card-body m-3">
-                    <h2 id="title" class="font-semibold text-white">{{ $contact->title }}</h2>
+                    <h2 id="title" class="font-semibold text-dark">{{ $contact->title }}</h2>
                     <div class="row mt-5">
-                        <div class="col-md-5">
+                        <div class="col-md-5" style="z-index: 5;">
                             <img class="w-40 contact_image float-right bg-white rounded-lg mr-3" src="{{ url($profile) }}" alt="profile image">
                         </div>
                         <div class="col-md-5 d-flex flex-column text-left my-auto text-white">
-                            <h3 id="name" class="mb-3">{{ $contact->name }}</h3>
-                            <h5 id="designation" class="mb-3">{{ $contact->designation }}</h5>
-                            <h5 id="address" class="mb-3">{{ $contact->address }}</h5>
-                            <h5 id="city_state"class="mb-3">{{ $contact->city }}, {{ $contact->pincode }}</h5>
+                            <h3 id="name" class="mb-3 text-danger">{{ $contact->name }}</h3>
+                            <h5 id="designation" class="mb-3 text-danger">{{ $contact->designation }}</h5>
+                            <h5 id="address" class="mb-3 text-danger">{{ $contact->address }}</h5>
+                            <h5 id="city_state"class="mb-3 text-danger">{{ $contact->city }}, {{ $contact->pincode }}</h5>
                         </div>
                     </div>
                     <div class="row mt-4 d-block">
                         <div class="row mt-2">
                             <div class="col-md-5">
-                                <i class="fa fa-phone-alt text-light float-right h5"></i>
+                                <i class="fa fa-phone-alt text-dark float-right h5"></i>
                             </div>
-                            <div class="col-md-5 text-left text-white my-auto">
+                            <div class="col-md-5 text-left text-dark my-auto">
                                 <h5 id="phone_no">{{ $contact->phone_no }}</h5>
                             </div>
                         </div>
@@ -69,9 +66,14 @@
                 </div>
             </div>
             <div class="mt-5 mx-auto">
-                <img class="winner_page_image mt-4 rounded-lg m-auto" src="{{ url('/images/School_Quiz_Logo.jpg') }}" alt="logo">
-                <button id="download_app" class="btn btn-primary py-2 px-3">Download App</button>
+               
+                
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('footer').hide();
+        });
+    </script>
 </x-app-layout>
